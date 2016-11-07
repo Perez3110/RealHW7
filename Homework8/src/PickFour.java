6bd413807ec42839b6a5c7e08e26ad3690304f06
unknown <>
2016-11-06 18:56:39 -0600

    initiated picking four program

---
+++ Homework8/src/PickFour.java
@@ -0,0 +1,13 @@
+public class PickFour {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hi There, This program is credited to \n "
+				+ "page 283 exercise 7.29 of Liang 10th edition");
+		
+		
+		
+
+	}
+
+}
